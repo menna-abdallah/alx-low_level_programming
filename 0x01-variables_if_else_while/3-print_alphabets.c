2ed6d3eda7b4942334fbdedd0d98ec4648318ab0
menna abdallah <menna.abdallh11@gmail.com>
2023-08-14 19:07:01 -0700

    mm

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabets low and up
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*print a -z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	 /*print A-Z*/
+	while (CH <= 'z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	printf("\n");
+	return (0);
+}
