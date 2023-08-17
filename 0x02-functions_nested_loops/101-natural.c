8560f8a720401e02dd5039186578736fdc903c23
menna abdallah <menna.abdallh11@gmail.com>
2023-08-16 18:25:17 -0700

    co

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point 
+ *
+ * Description: compute the sum of all the multiplies 
+ *		of 3 or 5 below 1024
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 ==0))
+			sum += num;
+	}
+	
+	printf("\n", num);
+	return (0);
+}
